116fa888c4673faeae9e01b89519d48466569ed9
박채연 <chaeyeonpark@outlook.sg>
2020-06-14 02:59:03 +0900

    pre_python_18.py answer

--- quiz/pre_python_18.py
+++ quiz/pre_python_18.py
@@ -14,3 +14,9 @@ print(new_list)
 ['exit', 'hi', 'playdata', 'intro']
 
 """
+
+file = ['exit.py','hi.py','playdata.hwp','intro.jpg']
+
+for i in file:
+    split = i.split(".")
+    print(split[0])
