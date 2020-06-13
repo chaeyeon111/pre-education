8dc4c354ee13a463156cd72c167cab34f42f5812
박채연 <chaeyeonpark@outlook.sg>
2020-06-14 02:45:07 +0900

    pre_python_14.py answer

--- quiz/pre_python_14.py
+++ quiz/pre_python_14.py
@@ -19,3 +19,37 @@ HELLO
 입력 형식이 잘못되었습니다.
 
 """
+
+string = str(input("Write : "))
+newstring = ''
+count1 = 0
+count2 = 0
+count3 = 0
+
+for a in string:
+    if (a.isupper()) == True:
+        count1 += 1
+        newstring +=(a.lower())
+
+    elif (a.islower()) == True:
+        count2 += 1
+        newstring += (a.upper())
+
+
+    elif (a.isspace()) == True:
+        count3 += 1
+        newstring += a
+
+    else:
+        print("입력 형식이 잘못되었습니다.")
+        break
+
+
+print("Uppercase -", count1)
+print("Lowercase -", count2)
+print("Spaces -", count3)
+print("Error -", len(string)- count1 -count2 - count3)
+
+print("After changing cases:")
+print(newstring)
+
