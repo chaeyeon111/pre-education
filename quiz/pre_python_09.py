ff2d074c521965b329f521dfb715dbd9812d0057
박채연 <chaeyeonpark@outlook.sg>
2020-06-14 02:19:49 +0900

    pre_python_09.py answer

--- quiz/pre_python_09.py
+++ quiz/pre_python_09.py
@@ -15,3 +15,17 @@ score : 88
 A
 
 """
+
+score = int(input("score : "))
+
+if score > 80:
+    print("grade is A")
+
+elif score > 60 :
+    print("grade is B")
+elif score > 40 :
+    print("grade is C")
+elif score > 20 :
+    print("grade is D")
+else :
+    print("grade is E")
