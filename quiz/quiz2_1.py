1c76b8dee1b38878d3353b6f551a018f754b2389
박채연 <chaeyeonpark@outlook.sg>
2020-06-14 20:46:09 +0900

    quiz2_1.py answer

--- quiz/quiz2_1.py
+++ quiz/quiz2_1.py
@@ -13,3 +13,29 @@ print(is_palindrome("토마토"))
 False
 True
 '''
+
+
+def is_palindrome(string):
+    if len(string) <= 1:
+        return True
+
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else:
+        return False
+
+
+def length(s):
+    for cut in range(len(s), 0, -1):
+        for start in range(0, len(s)):
+            cutStr = s[start:start + cut]
+            # 자른 string 전달
+            if is_palindrome(cutStr) == True:
+                return len(cutStr)
+
+            if start + cut >= len(s):
+                break
+
+
+print(is_palindrome("radio"))
+print(is_palindrome("토마토"))
