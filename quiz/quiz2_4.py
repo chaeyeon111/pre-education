9c9c6b821ce73b0cc40f6fd290bafa684c8aa581
박채연 <chaeyeonpark@outlook.sg>
2020-06-14 21:07:18 +0900

    quiz2_4.py answer

--- quiz/quiz2_4.py
+++ quiz/quiz2_4.py
@@ -10,9 +10,9 @@
 <입력>
 multi_card=Multi_card()
 multi_card.charge(20000)
-multi_card.consume(5000,'마트')
-multi_card.consume(10000,'영화관')
-multi_card.consume(2000,'교통')
+multi_card.movie(5000,'마트')
+multi_card.movie(10000,'영화관')
+multi_card.movie(2000,'교통')
 multi_card.print()
 
 <출력>
@@ -23,3 +23,61 @@ multi_card.print()
 교통에서 1800.0원을 사용했습니다.
 잔액이 5700.0원 입니다
 '''
+
+
+class Multi_card:
+    def __init__(self):
+        self.money = 0
+        print("카드가 발급되었습니다")
+
+    def charge(self, num):
+        self.money += num
+        print(str(num) + "가 충전되었습니다.")
+
+    def movie(self, num, x):
+
+        if x == '영화관':
+            if self.money - num * 0.8 < 0:
+                print("잔액이 부족합니다.")
+
+            else:
+                self.money -= num * 0.8
+                print("{}에서 {:.0f}원 사용했습니다.".format(x, num * 0.8))
+
+        elif x == '마트':
+            if self.money - num * 0.9 < 0:
+                print("잔액이 부족합니다.")
+
+            else:
+                self.money -= num * 0.9
+                print("{}에서 {:.0f}원 사용했습니다.".format(x, num * 0.9))
+
+        elif x == '교통':
+            if self.money - num * 0.9 < 0:
+                print("잔액이 부족합니다.")
+
+            else:
+                self.money -= num * 0.9
+                print("{}에서 {:.0f}원 사용했습니다.".format(x, num * 0.9))
+
+
+
+
+        else:
+            if self.money - num < 0:
+                print("잔액이 부족합니다.")
+
+            else:
+                self.money -= num
+                print("{}에게 {:.0f}원 사용했습니다.".format(x, num))
+
+    def print(self):
+        print('잔액이 {:.0f}입니다.'.format(self.money))
+
+
+multi_card = Multi_card()
+multi_card.charge(20000)
+multi_card.movie(5000, '마트')
+multi_card.movie(10000, '영화관')
+multi_card.movie(2000, '교통')
+multi_card.print()
